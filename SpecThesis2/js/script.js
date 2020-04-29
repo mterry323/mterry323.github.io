dab54e3060e1c73f05148538325a0efcee0fa4dc
Micaela <mterry323@users.noreply.github.com>
2020-04-28 22:08:54 -0700

    did i break it tho

--- SpecThesis2/js/script.js
+++ SpecThesis2/js/script.js
@@ -1,45 +1,54 @@
 $(document).ready(function() {
-  // Make the DIV element draggable:
-  dragElement(document.getElementById("thumb"));
+  // Wrap the module in a self-executing anonymous function
+// to avoid leaking variables into global scope:
+(function (document) {
+    // Enable ECMAScript 5 strict mode within this function:
+    'use strict';
 
-  function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-      // if present, the header is where you move the DIV from:
-      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-      // otherwise, move the DIV from anywhere inside the DIV:
-      elmnt.onmousedown = dragMouseDown;
+    // This function initializes the drag of an element where an
+    // event ("mousedown") has occurred:
+    function startDrag(evt) {
+
+        // The element's position is based on its top left corner,
+        // but the mouse coordinates are inside of it, so we need
+        // to calculate the positioning difference:
+        var diffX = evt.clientX - this.offsetLeft,
+            diffY = evt.clientY - this.offsetTop,
+            that = this; // "this" refers to the current element,
+                         // let's keep it in cache for later use.
+
+        // moveAlong places the current element (referenced by "that")
+        // according to the current cursor position:
+        function moveAlong(evt) {
+            that.style.left = (evt.clientX - diffX) + 'px';
+            that.style.top = (evt.clientY - diffY) + 'px';
+        }
+
+        // stopDrag removes event listeners from the element,
+        // thus stopping the drag:
+        function stopDrag() {
+            document.removeEventListener('mousemove', moveAlong);
+            document.removeEventListener('mouseup', stopDrag);
+        }
+
+        document.addEventListener('mouseup', stopDrag);
+        document.addEventListener('mousemove', moveAlong);
     }
 
-    function dragMouseDown(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // get the mouse cursor position at startup:
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = elementDrag;
+    // Performs a check if the current element is draggable and if yes,
+    // then the dragging is initiated:
+    function startDragIfDraggable(evt) {
+        // Check if the target element (referenced by evt.target) contains a
+        // class named "draggable" (http://stackoverflow.com/questions/5898656/):
+        if (evt.target.classList.contains('draggable')) {
+            // Invoke startDrag by passing it the target element as "this":
+            startDrag.call(evt.target, evt);
+        }
     }
 
-    function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // calculate the new cursor position:
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      // set the element's new position:
-      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
+    // Listen for any "mousedown" event on the document.body and attempt dragging
+    // the target element (the one where "mousedown" occurred) if it's draggable:
+    document.body.addEventListener('mousedown', startDragIfDraggable);
 
-    function closeDragElement() {
-      // stop moving when mouse button is released:
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
   }
 });
