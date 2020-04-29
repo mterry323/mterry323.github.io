df177c2f7b05512bf60713b9d3d78ed211979e26
Micaela <mterry323@users.noreply.github.com>
2020-04-28 19:03:11 -0700

    prototype2

--- SpecThesis/script.js
+++ SpecThesis/script.js
@@ -1,12 +1,46 @@
 $(document).ready(function() {
 
-  //get list of divs
-  var div = $(".thumb").toArray();
+  // Make the DIV element draggable:
+dragElement(document.getElementById("thumb"));
 
-  //randomly print them back out.
-  while (div.length > 0) {
-    var idx = Math.floor((Math.random() * (div.length - 1)));
-    var element = div.splice(idx, 1);
-    $('body').append(element[0]);
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.class + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.class + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
   }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 });
