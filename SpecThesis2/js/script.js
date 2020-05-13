e3ae81f720f7b4fbf9a7267a7f1a51cafe33e4cf
Micaela <mterry323@users.noreply.github.com>
2020-05-13 12:56:30 -0700

    popups

--- SpecThesis2/js/script.js
+++ SpecThesis2/js/script.js
@@ -1,54 +1,31 @@
 $(document).ready(function() {
-  // Wrap the module in a self-executing anonymous function
-// to avoid leaking variables into global scope:
-(function (document) {
-    // Enable ECMAScript 5 strict mode within this function:
-    'use strict';
-
-    // This function initializes the drag of an element where an
-    // event ("mousedown") has occurred:
-    function startDrag(evt) {
-
-        // The element's position is based on its top left corner,
-        // but the mouse coordinates are inside of it, so we need
-        // to calculate the positioning difference:
-        var diffX = evt.clientX - this.offsetLeft,
-            diffY = evt.clientY - this.offsetTop,
-            that = this; // "this" refers to the current element,
-                         // let's keep it in cache for later use.
-
-        // moveAlong places the current element (referenced by "that")
-        // according to the current cursor position:
-        function moveAlong(evt) {
-            that.style.left = (evt.clientX - diffX) + 'px';
-            that.style.top = (evt.clientY - diffY) + 'px';
-        }
-
-        // stopDrag removes event listeners from the element,
-        // thus stopping the drag:
-        function stopDrag() {
-            document.removeEventListener('mousemove', moveAlong);
-            document.removeEventListener('mouseup', stopDrag);
-        }
-
-        document.addEventListener('mouseup', stopDrag);
-        document.addEventListener('mousemove', moveAlong);
-    }
-
-    // Performs a check if the current element is draggable and if yes,
-    // then the dragging is initiated:
-    function startDragIfDraggable(evt) {
-        // Check if the target element (referenced by evt.target) contains a
-        // class named "draggable" (http://stackoverflow.com/questions/5898656/):
-        if (evt.target.classList.contains('draggable')) {
-            // Invoke startDrag by passing it the target element as "this":
-            startDrag.call(evt.target, evt);
-        }
-    }
-
-    // Listen for any "mousedown" event on the document.body and attempt dragging
-    // the target element (the one where "mousedown" occurred) if it's draggable:
-    document.body.addEventListener('mousedown', startDragIfDraggable);
-
-  }
+  //
+  $('a.poplight[href^=#]').click(function() {
+    var popID = $(this).attr('rel'); //Get Popup Name
+    var popURL = $(this).attr('href'); //Get Popup href to define size
+    var query = popURL.split('?');
+    var dim = query[1].split('&');
+    var popWidth = dim[0].split('=')[1]; //Gets the first query string value
+    $('#' + popID).fadeIn().css({
+      'width': Number(popWidth)
+    }).prepend('<a href="#" class="close"></a>');
+    var popMargTop = ($('#' + popID).height() + 80) / 2;
+    var popMargLeft = ($('#' + popID).width() + 80) / 2;
+    //Apply Margin to Popup
+    $('#' + popID).css({
+      'margin-top': -popMargTop,
+      'margin-left': -popMargLeft
+    });
+    $('body').append('<div id="fade"></div>');
+    $('#fade').css({
+      'filter': 'alpha(opacity=80)'
+    }).fadeIn(); //Fade in the fade layer - .css({'filter' : 'alpha(opacity=80)'})
+    return false;
+  });
+  $('a.close, #fade').live('click', function() {
+    $('#fade , .popup_block').fadeOut(function() {
+      $('#fade, a.close').remove(); //fade them both out
+    });
+    return false;
+  });
 });
